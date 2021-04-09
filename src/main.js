74e528e5adfd5e256ce0994206ea08a73ee27896
Vinícius Santana <v-silva-s@hotmail.com>
2021-04-09 15:04:38 -0300

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+
+import BoostrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
+
+Vue.use(BoostrapVue)
 
 Vue.config.productionTip = false
 
